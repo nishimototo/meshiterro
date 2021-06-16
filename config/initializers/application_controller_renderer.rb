79260d32cbcf0162915344d046a2403a68883f9c
nishimototo <dkspm2436@gmail.com>
2021-06-16 23:41:05 +0000

    投稿一覧まで実装

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b700c10a5261c8a2b494143005f8aa948b99e827c0eb21b6c9987d6bb25358ba6f142e3e6f34d421bdc20ca89b0d7358adc774fe9bd998946d0d1f88ea119b88'
